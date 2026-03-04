c239f4b830d6040392e28c1c6f0cd0dcf7eadf56
Amir Rehman Marwat <amirrehmanmwt@gmail.com>
2026-03-04 00:26:26 -0800

    added the pdf to ttex feauture in the api

--- Backend/controllers/interview.controllers.js
+++ Backend/controllers/interview.controllers.js
@@ -1,24 +1,29 @@
-
-import pdfparser from "pdf-parser"
 import generateAireport from "../services/report.ai.js";
 import AiReportModel from "../models/AiReportSchema.js";
-import pdfParser from "pdf-parser";
-export const interviewReportController =async(req,res)=>{
+import { PDFParse } from 'pdf-parse';
+export const interviewReportController = async (req, res) => {
     
-const resume=req.file
-const resumeText= new pdfParser
-const {selfDescription,jobDescription}=req.body;
-const user=req.user
-const aiReport=await generateAireport(selfDescription,resumeText,jobDescription)
-const savedReport=await AiReportModel.create({
-    User:user.id,
-    selfDescription,
-    Resume:resumeText,
-    jobDescription,
-    ...aiReport
-})
+     const resume = req.file;
+const parser = new PDFParse({ data:resume.buffer});
+const result = await parser.getText();
+const resumeText=result.text
+console.log(resumeText)
+res.json({baw:resumeText})
 
-return res
-          .status(201) 
-          .json({message:"Report generated successfully",aiReport})
+
+    // const {selfDescription,jobDescription}=req.body;
+
+    // const user=req.user
+    // const aiReport=await generateAireport(selfDescription,resumeText,jobDescription)
+    // const savedReport=await AiReportModel.create({
+    //     User:user.id,
+    //     selfDescription,
+    //     Resume:resumeText, 
+    //     jobDescription,
+    //     ...aiReport
+    // })
+
+    // return res
+    //           .status(201) 
+    //           .json({message:"Report generated successfully",aiReport})
 }
